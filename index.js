320d0ac6904d2059eba4b7f85adad307d21f920e
Rajiv Ranjan <67179338+rajivranjanmars@users.noreply.github.com>
2021-09-14 21:19:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function myLink() {
   let a = document.getElementById("number")
   const b = document.getElementById("url")
   tabX(a.value -1, b.value);
-    window.open(b.value,"_self");
+    window.open(b.value,"_blank");
     window.open("index.html");
   
 }
